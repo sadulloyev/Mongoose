d8eeb3d9e8689f6b66ac1aa8d2a10226c73aa0b7
shukhrat <sadullayevsh24@gmail.com>
2023-07-28 15:14:11 +0500

    m

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB');
+// mongoose.connect('mongodb://127.0.0.1:27017/personDB');
 
 const fruitSchema = new mongoose.Schema({
     name: String,
@@ -13,61 +11,74 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 async function saveFruit() {
-    const existingFruit = await Fruit.findOne({ name: "Apple" });
-
-    if (!existingFruit) {
-        const fruit = new Fruit({
-            name: "Apple",
-            rating: 7,
-            review: "Pretty solid as a fruit."
-        });
-
-        try {
-            await fruit.save();
-            console.log('Fruit saved successfully!');
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        console.log('Fruit already exists in the database.');
+    const fruit = new Fruit({
+        name: "Apple",
+        rating: 7,
+        review: "Pretty solid as a fruit."
+    });
+    try {
+        await fruit.save();
+        console.log('Fruit saved successfully!');
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // mongoose.disconnect(); // Close the database connection
     }
 }
-
 const personSchema = new mongoose.Schema({
     name: String,
     age: Number,
-});
 
+});
 const Person = mongoose.model("Person", personSchema);
 
 async function savePerson() {
-    const existingPerson = await Person.findOne({ name: "John" });
+    const person = new Person({
+        name: "John",
+        age: 34,
+    });
 
-    if (!existingPerson) {
-        const person = new Person({
-            name: "John",
-            age: 34,
-        });
-
-        try {
-            await person.save();
-            console.log('Person saved successfully!');
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        console.log('Person already exists in the database.');
+    try {
+        await person.save();
+        console.log('Person saved successfully!');
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // mongoose.disconnect(); // Close the database connection
     }
 }
 
-async function run() {
-    await saveFruit();
-    await savePerson();
-    mongoose.disconnect();
-}
+const kiwi = new Fruit(
+    {
+        name: "Kiwi",
+        rating: 10,
+        review: "Pretty solid as a fruit."
+    });
 
-run();
+const banana = new Fruit({
+    name: "Banana",
+    rating: 8,
+    review: "nider fruit"
+});
+// Fruit.insertMany([kiwi, banana])
+//     .then(() => {
+//         console.log("successfully saved");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 
+Fruit.find(function (err, fruits) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(fruits);
+    }
+});
+
+
+saveFruit();
+savePerson();
 
 
 
